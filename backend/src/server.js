0151a44beaa0e9d9d179d4203663444c1555d080
Loc <165229061+loc2k3iuh@users.noreply.github.com>
2025-08-25 22:18:36 +0700

    db connected & env.js completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
-import dotenv from "dotenv";
 import { ENV } from "./config/env.js";
-dotenv.config();
+import { connectDB } from "./config/db.js";
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World !");
-})
+  res.send("Hello World !");
+});
 console.log("Mongo Uri: ", ENV.MONGO_URI);
-app.listen(ENV.PORT, () => console.log("Server started on port:", ENV.PORT));
+app.listen(ENV.PORT, () => {
+  console.log("Server started on port:", ENV.PORT)
+  connectDB();
+});
