3c1bad5322597b3ab5252c68bd5934a00facfd61
Loc <165229061+loc2k3iuh@users.noreply.github.com>
2025-08-27 14:00:36 +0700

    set up stream and sentry

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "slack-clone" });
@@ -26,6 +27,12 @@ const syncUser = inngest.createFunction(
 
     console.log("Creating user with data:", newUser);
     await User.create(newUser);
+
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.image,
+    });
   }
 );
 
@@ -34,7 +41,8 @@ const deleteUserFromDB = inngest.createFunction(
   { event: "clerk/user.deleted" },
   async ({ event }) => {
     const { id } = event.data;
-    await User.deleteOne({ clerkId: id });
+    await User.deleteOne({ clerkId: id }); 
+    await deleteStreamUser(id.toString());
   }
 );
 
