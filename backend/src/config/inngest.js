0668a707862a298af5bef6a09acdd1f89f05a37a
Loc <165229061+loc2k3iuh@users.noreply.github.com>
2025-08-27 10:47:54 +0700

    update email feild

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -5,34 +5,38 @@ import { User } from "../models/user.model.js";
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "slack-clone" });
 
-
 const syncUser = inngest.createFunction(
-    {id: "sync-user"},
-    {event: "clerk/user.created"},
-    async ({event}) => {
-        await connectDB();
+  { id: "sync-user" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDB();
 
-        const {id, email_addresses, first_name, last_name, image_url} = event.data;
+    const { id, email_addresses, first_name, last_name, image_url } =
+      event.data;
 
-        const newUser = {
-            clerkId: id,
-            email: email_addresses[0]?.email_addresses,
-            name: `${first_name || ""} ${last_name || ""}`,
-            image: image_url || "",
-        };
+    const newUser = {
+      clerkId: id,
+      email:
+        email_addresses && email_addresses.length > 0
+          ? email_addresses[0].email_address
+          : undefined,
+      name: `${first_name || ""} ${last_name || ""}`,
+      image: image_url || "",
+    };
 
-        await User.create(newUser);
-    }
+    console.log("Creating user with data:", newUser);
+    await User.create(newUser);
+  }
 );
 
 const deleteUserFromDB = inngest.createFunction(
-    {id: "delete-user-from-db"},
-    {event: "clerk/user.deleted"},
-    async ({event}) => {
-        const { id } = event.data;
-        await User.deleteOne({clerkId: id});
-    }
-)
+  { id: "delete-user-from-db" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
+  }
+);
 
 // Create an empty array where we'll export future Inngest functions
 export const functions = [syncUser, deleteUserFromDB];
