6dcb14bc20ee2429f6fb1b763c4062550305e32d
Loc <165229061+loc2k3iuh@users.noreply.github.com>
2025-09-04 15:15:10 +0700

    fix typo .env

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 // const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5001/api' : 'https://chat-video-call-app-backend-git-main-loc2k3iuhs-projects.vercel.app/api';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URl;
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const axiosInstance = axios.create({
  baseURL: BASE_URL,
